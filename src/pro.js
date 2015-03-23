dca234f03576d0cc52515989ecaca7c1396f1a98
Vlad Semenov <vladislav.semenov@gmail.com>
2015-03-24 00:44:15 +0600

    Added sketches for some methods

--- src/pro.js
+++ src/pro.js
@@ -14,10 +14,80 @@ function delay(ms, value) {
 	});
 }
 
+function resolve(val) {
+	return Promise.resolve(val);
+}
+
+function reject(val) {
+	return Promise.reject(val);
+}
+
+function race(array) {
+	return Promise.race(array);
+}
+
+function try(fn) {
+	try {
+		return resolve(fn());
+	} catch (e) {
+		return reject(e);
+	}
+}
+
+function each(array, fn) {
+	array.forEach(function(val, i) {
+
+	});
+}
+
+function whilst(test, fn) {
+	if (test()) {
+		return fn().then(function() {
+			return whilst(test, fn);
+		});
+	}
+}
+
+function series(fns) {
+	var i = 0;
+	var res = [];
+	var fn;
+	return whilst(function() {
+		return i < fns.length;
+	}, function() {
+		fn = fns[i];
+		return fn().then(function(val) {
+			res[i] = val;
+			i++;
+		})
+	}).then(function() {
+		return res;
+	});
+}
+
+/*
+	try
+	reject
+	resolve
+	race
+	any
+	series
+	parallelLimit?
+	whilst/until
+	nextTick?
+	each?
+	reduce?
+	nodeify? promise to callback
+*/
+
 var pro = {
+	resolve: resolve,
+	reject: reject,
 	all: all,
+	parallel: all,
 	map: map,
-	delay: delay
+	delay: delay,
+	try: try
 };
 
 module.exports = pro;
